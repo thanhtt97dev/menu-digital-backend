56998c7a7a5374ba8d1a3045038b1aa47f4bbb2c
hiuhihi78 <leduchieu2001x@gmail.com>
2025-03-06 00:30:18 +0700

    [Configuration] handle Global Exception middleware

---
+++ app/common/shared/exceptions/base/not_found_exception.py
@@ -0,0 +1,8 @@
+from app.common.shared.exceptions.base.domain_exception import DomainException
+
+class NotFoundException(DomainException):
+
+    def __init__(self, title, message):
+        super().__init__(title, message)
+        self.title = title,
+        self.message = message
