bda08c65b29cc4c0a1451e5b1a29d5388d2ec7fe
hiuhihi78 <leduchieu2001x@gmail.com>
2025-03-29 14:05:00 +0700

    [Base] update config api versionning

--- app/routers/v1/auth_route.py
+++ app/routers/v1/auth_route.py
@@ -15,14 +15,17 @@ from app.common.shared.configurations.enviroment_config import enviroment
 router = APIRouter(prefix="/auth", tags=["Authentication"])
 
 @router.post("/signup")
+@version(enviroment.API_VERSION)
 def sign_up(sign_up_data: SignUp, db: Session = Depends(get_db)):
     return AuthService(db).sign_up(sign_up_data)
     
     
 @router.post('/signin')
+@version(enviroment.API_VERSION)
 def sign_in(sign_data: SignIn, db: Session = Depends(get_db)):
     return AuthService(db).sign_in(sign_data)
 
 @router.post('/SignInByGoogle')
+@version(enviroment.API_VERSION)
 def sign_in_by_google(sign_in_google_data: SignInByGoogle, db: Session = Depends(get_db)):
     return AuthService(db).sign_in_by_google(sign_in_google_data)
--- app/routers/v1/role_route.py
+++ app/routers/v1/role_route.py
@@ -7,15 +7,16 @@ from app.services.v1.role_service import RoleService
 from app.schemas.role_schema import  RoleCreate, RoleResponse
 from app.common.base.authorize import authorize
 from app.common.shared.constants.app_constant import AppConstants
+from app.common.shared.configurations.enviroment_config import enviroment
 
 router = APIRouter(prefix="/roles", tags=["Roles"])
 
 @router.post("", response_model= RoleResponse)
-@version(1)
+@version(enviroment.API_VERSION)
 def create_role(role_data: RoleCreate, db: Session = Depends(get_db), user = Depends(authorize([AppConstants.Role.Admin]))):
     return RoleService.create_role(db, role_data)
     
 @router.get('')
-@version(1)
+@version(enviroment.API_VERSION)
 def get_all_roles(db:Session = Depends(get_db)):
     return RoleService.get_all_roles(db)
