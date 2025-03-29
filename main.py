ee528f7c09486321104ac75133b1b68e3504f60f
hiuhihi78 <leduchieu2001x@gmail.com>
2025-03-29 14:08:11 +0700

    fastapi_versioning lib is not support using custom middleware -> ignor using api versionning

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ async def on_startup():
     pass
 
 if __name__ == '__main__':
-    uvicorn.run(versioned_app, host='0.0.0.0', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=8000)
     # uvicorn.run("main:app", host='0.0.0.0', port=8000, reload=True)
     
 
